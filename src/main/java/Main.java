53c2bc488a3f170cf1afb2f6213df8d1b217db7a
KiranjotKaur88 <kiranjotk24@gamil.com>
2021-04-10 15:25:25 -0400

    Updated Hello Vivek

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 public class Main {
     public static void main{String[] args}
-    system.out.println("hello World")
+    System.out.println("hello World");
+    System.out.println("hello Vivek");
 }
