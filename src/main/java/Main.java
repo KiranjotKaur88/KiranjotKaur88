f7294ffd8fe02eea31c58be6c778ac74a7e61871
poojanehra <poojanehra444@gmail.com>
2021-04-13 18:13:07 -0400

    Pooja message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main{String[] args}
     System.out.println("hello World");
     System.out.println("hello Vivek");
-    System.out.println("hello");
     System.out.println("hello Everyone");
+    System.out.println("welcome to India");
 }
