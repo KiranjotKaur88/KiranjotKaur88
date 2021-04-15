e290a61e093f92b0b2f2d16428f01c33be65924f
KiranjotKaur88 <kiranjotk24@gmail.com>
2021-04-15 17:50:23 -0400

    making changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,9 @@ public class Main {
     public static void main{String[] args}
     System.out.println("hello World");
     System.out.println("hello Vivek");
-<<<<<<< HEAD
 System.out.println("Practice time")
-=======
-    System.out.println("welcome");
 
->>>>>>> 385422abc71905d66a815493d51db5a38c0f07b5
+    System.out.println("welcome");
+ System.out.println("Hi");
+
 }
